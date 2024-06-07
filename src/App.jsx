c7f594d6ef8e4e2d077b1c8edba48af360cbd28b
Danil <deltayt.ro@gmail.com>
2024-06-07 19:18:50 +0300

    1.0

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import ParticlesComponent from './components/libraries/particles.jsx'
+
+function App() {
+  return (
+    <>
+      <div className="particles">
+        <ParticlesComponent />
+      </div>
+    </>
+  )
+}
+
+export default App
